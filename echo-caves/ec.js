1c7b713396c9fa46e733af4c523f0784d43df959
ReactivePeak <ReactivePeak@users.noreply.github.com>
2020-12-05 00:18:54 -0800

    initializing the EC catalog

--- echo-caves/ec.js
+++ echo-caves/ec.js
@@ -4,18 +4,21 @@ function Scrolled() {
     document.getElementById("staticMenu").style.visibility = "hidden";
     document.getElementById("floatMenu").style.visibility = "visible";
     document.getElementById("SMEchoCaves").style.opacity = "0";
+    document.getElementById("SMCatalog").style.opacity = "0";
     document.getElementById("SMTp").style.opacity = "0";
     document.getElementById("SMCl").style.opacity = "0";
     document.getElementById("SMAbout").style.opacity = "0";
   }
   if(floatCalled > 150) {
     document.getElementById("FMEchoCaves").style.opacity = "1";
+    document.getElementById("FMCatalog").style.opacity = "1";
     document.getElementById("FMTp").style.opacity = "1";
     document.getElementById("FMCl").style.opacity = "1";
     document.getElementById("FMAbout").style.opacity = "1";
   }
   if(floatCalled < 150) {
     document.getElementById("FMEchoCaves").style.opacity = "0";
+    document.getElementById("FMCatalog").style.opacity = "0";
     document.getElementById("FMTp").style.opacity = "0";
     document.getElementById("FMCl").style.opacity = "0";
     document.getElementById("FMAbout").style.opacity = "0";
@@ -24,6 +27,7 @@ function Scrolled() {
     document.getElementById("floatMenu").style.visibility = "hidden";
     document.getElementById("staticMenu").style.visibility = "visible";
     document.getElementById("SMEchoCaves").style.opacity = "1";
+    document.getElementById("SMCatalog").style.opacity = "1";
     document.getElementById("SMTp").style.opacity = "1";
     document.getElementById("SMCl").style.opacity = "1";
     document.getElementById("SMAbout").style.opacity = "1";
