d1d9f24cdaa72938feafd0ce657ffcabbce55672
ReactivePeak <ReactivePeak@users.noreply.github.com>
2020-12-31 14:53:48 -0800

    insert google verification HTML for google extension

--- echo-caves/themepunk/Thp-Scripts.js
+++ echo-caves/themepunk/Thp-Scripts.js
@@ -30,38 +30,7 @@ var jsonData = JSON.stringify(json);
 var themeData = JSON.parse(jsonData);
 
 function Update() {
-    
-    //Update Function Initializations
-    var PrimaryBGColor = document.getElementById("BGColor1").value;
-    var SecondaryBGColor = document.getElementById("BGColor2").value;
-    var TertiaryBGColor = document.getElementById("BGColor3").value;
-    var QuaternaryBGColor = document.getElementById("BGColor4").value;
-    var QuinaryBGColor = document.getElementById("BGColor5").value;   
-    var PrimaryTextColor = document.getElementById("TextColor1").value;
-    var SecondaryTextColor = document.getElementById("TextColor2").value;
-    var TertiaryTextColor= document.getElementById("AccentTextColor1").value;
-    var AccentTextColor = document.getElementById("AccentTextColor2").value;
-    var PrimaryGradeColor = document.getElementById("GradeColor1").value;
-    var SecondaryGradeColor = document.getElementById("GradeColor2").value;
-    var TertiaryGradeColor = document.getElementById("GradeColor3").value;
-    var PrimaryMenuColor= document.getElementById("MenuColor1").value;
-    var SecondaryMenuColor = document.getElementById("MenuColor2").value;
-
-    //JSON Object Assignments
-    themeData.Background.NewPrimaryBackgroundColor = PrimaryBGColor;
-    themeData.Background.NewSecondaryBackgroundColor = SecondaryBGColor;
-    themeData.Background.NewTertiaryBackgroundColor = TertiaryBGColor;
-    themeData.Background.NewQuaternaryBackgroundColor = QuaternaryBGColor;
-    themeData.Background.NewQuinaryBackgroundColor = QuinaryBGColor;    
-    themeData.Text.NewPrimaryTextColor = PrimaryTextColor;
-    themeData.Text.NewSecondaryTextColor = SecondaryTextColor;
-    themeData.Text.NewPrimaryAccentTextColor= TertiaryTextColor;
-    themeData.Text.NewSecondaryAccentTextColor = AccentTextColor; 
-    themeData.Grade.NewPrimaryGradeColor = PrimaryGradeColor;
-    themeData.Grade.NewSecondaryGradeColor = SecondaryGradeColor;
-    themeData.Grade.NewTertiaryGradeColor = TertiaryGradeColor;
-    themeData.Menu.NewPrimaryMenuColor = PrimaryMenuColor;
-    themeData.Menu.NewSecondaryMenuColor = SecondaryMenuColor;
+    writeJSON();
     //CSS Property Assignments
     document.documentElement.style.setProperty("--BackgroundColor1", themeData.Background.NewPrimaryBackgroundColor);
     document.documentElement.style.setProperty("--BackgroundColor2", themeData.Background.NewSecondaryBackgroundColor);
@@ -78,41 +47,8 @@ function Update() {
     document.documentElement.style.setProperty("--MenuColor1", themeData.Menu.NewPrimaryMenuColor);
     document.documentElement.style.setProperty("--MenuColor2", themeData.Menu.NewSecondaryMenuColor);
 }
-
 function Confirm() {
-
-    //HTML Element Value Initializations
-    var PrimaryBGColor = document.getElementById("BGColor1").value;
-    var SecondaryBGColor = document.getElementById("BGColor2").value;
-    var TertiaryBGColor = document.getElementById("BGColor3").value;
-    var QuaternaryBGColor = document.getElementById("BGColor4").value;
-    var QuinaryBGColor = document.getElementById("BGColor5").value;
-    var PrimaryTextColor = document.getElementById("TextColor1").value;
-    var SecondaryTextColor = document.getElementById("TextColor2").value;
-    var TertiaryTextColor= document.getElementById("AccentTextColor1").value;
-    var AccentTextColor = document.getElementById("AccentTextColor2").value;
-    var PrimaryGradeColor = document.getElementById("GradeColor1").value;
-    var SecondaryGradeColor = document.getElementById("GradeColor2").value;
-    var TertiaryGradeColor = document.getElementById("GradeColor3").value;
-    var PrimaryMenuColor= document.getElementById("MenuColor1").value;
-    var SecondaryMenuColor = document.getElementById("MenuColor2").value;
-
-    //JSON Object Assignments
-    themeData.Background.NewPrimaryBackgroundColor = PrimaryBGColor;
-    themeData.Background.NewSecondaryBackgroundColor = SecondaryBGColor;
-    themeData.Background.NewTertiaryBackgroundColor = TertiaryBGColor;
-    themeData.Background.NewQuaternaryBackgroundColor = QuaternaryBGColor;
-    themeData.Background.NewQuinaryBackgroundColor = QuinaryBGColor;
-    themeData.Text.NewPrimaryTextColor = PrimaryTextColor;
-    themeData.Text.NewSecondaryTextColor = SecondaryTextColor;
-    themeData.Text.NewPrimaryAccentTextColor= TertiaryTextColor;
-    themeData.Text.NewSecondaryAccentTextColor = AccentTextColor;
-    themeData.Grade.NewPrimaryGradeColor = PrimaryGradeColor;
-    themeData.Grade.NewSecondaryGradeColor = SecondaryGradeColor;
-    themeData.Grade.NewTertiaryGradeColor = TertiaryGradeColor;
-    themeData.Menu.NewPrimaryMenuColor = PrimaryMenuColor;
-    themeData.Menu.NewSecondaryMenuColor = SecondaryMenuColor;
-
+    writeJSON();
     //Copy All JSON Data To Clipboard
     var JsonThemeData = JSON.stringify(themeData);
     var rawThemeData = document.createElement('textarea');
@@ -123,4 +59,21 @@ function Confirm() {
     rawThemeData.style = {position: 'absolute', left: '-9999px'};
     document.execCommand('copy');
     document.body.removeChild(rawThemeData);
+}
+
+function writeJSON() {
+    themeData.Background.NewPrimaryBackgroundColor = document.getElementById("BGColor1").value;
+    themeData.Background.NewSecondaryBackgroundColor = document.getElementById("BGColor2").value;
+    themeData.Background.NewTertiaryBackgroundColor = document.getElementById("BGColor3").value;
+    themeData.Background.NewQuaternaryBackgroundColor = document.getElementById("BGColor4").value;
+    themeData.Background.NewQuinaryBackgroundColor = document.getElementById("BGColor5").value;    
+    themeData.Text.NewPrimaryTextColor = document.getElementById("TextColor1").value;
+    themeData.Text.NewSecondaryTextColor = document.getElementById("TextColor2").value;
+    themeData.Text.NewPrimaryAccentTextColor= document.getElementById("AccentTextColor1").valuer;
+    themeData.Text.NewSecondaryAccentTextColor = document.getElementById("AccentTextColor2").value; 
+    themeData.Grade.NewPrimaryGradeColor = document.getElementById("GradeColor1").value;
+    themeData.Grade.NewSecondaryGradeColor = document.getElementById("GradeColor2").value;
+    themeData.Grade.NewTertiaryGradeColor = document.getElementById("GradeColor3").value;
+    themeData.Menu.NewPrimaryMenuColor = document.getElementById("MenuColor1").value;
+    themeData.Menu.NewSecondaryMenuColor = document.getElementById("MenuColor2").value;
 }
