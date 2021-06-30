1962c57957f3b304f14c6031a3df232563295da3
ReactivePeak <reactivepeakstudios@gmail.com>
2021-06-30 02:18:44 -0700

    + Herozitus Stats (API WOOOOOO)

--- herozitus/hz-functions.js
+++ herozitus/hz-functions.js
@@ -1,3 +1,18 @@
 function addHeroZitus() {
     window.location.href = 'https://discord.com/api/oauth2/authorize?client_id=733952380772679763&permissions=0&scope=bot';
+    document.getElementById("server-ct").innerHTML = `Servers ${data}`
 }
+
+var jsonData;
+var objData;
+
+fetch('https://herozitus.herokuapp.com/')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    document.getElementById('server-ct').innerHTML = (Math.round(JSON.parse(data).servers * 100) / 100).toLocaleString();
+    document.getElementById('member-ct').innerHTML = (Math.round(JSON.parse(data).globalMemberCt * 100) / 100).toLocaleString();
+  });
+
+  console.log(jsonData);
