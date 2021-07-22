2d5f38ad4c01b1f56eec501af053b7f4e53236f4
ReactivePeak <reactivepeakstudios@gmail.com>
2021-07-22 15:29:07 -0700

    highlight feature

--- herozitus/hz-functions.js
+++ herozitus/hz-functions.js
@@ -1,8 +1,8 @@
 function addHeroZitus() {
     window.location.href = 'https://discord.com/api/oauth2/authorize?client_id=733952380772679763&permissions=0&scope=bot';
 }
-var jsonData;
-var objData;
+let jsonData;
+let objData;
 fetch('https://herozitus.herokuapp.com/')
   .then((response) => {
     return response.json();
