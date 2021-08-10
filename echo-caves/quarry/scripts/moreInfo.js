c237f86d9034b9ab2f07ce4281812a6fabee6126
ReactivePeak <reactivepeakstudios@gmail.com>
2021-08-09 23:29:08 -0700

    + Magic 0Auth | change catalog to quarry | navigation fixes
    Almost ready for production, requires 50 themes before announcing to the public.

---
+++ echo-caves/quarry/scripts/moreInfo.js
@@ -0,0 +1,10 @@
+document.getElementById("moreInfo-volcano").addEventListener( "click", 
+    function() {
+        window.open('./theme/volcano/volcano.html','_self',false)
+    }
+);
+document.getElementById("moreInfo-whisps").addEventListener( "click", 
+    function() {
+        window.open('./theme/whisps/whisps.html','_self',false)
+    }
+);
---
+++ echo-caves/quarry/theme/whisps/whisps.js
@@ -0,0 +1,3 @@
+document.getElementById("all-themes").addEventListener("click", function() { window.open ('/index.html','_self',false)});
+
+document.getElementById("star5").addEventListener("click", function() { });
